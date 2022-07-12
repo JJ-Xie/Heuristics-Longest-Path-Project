bd9250e619e7b6d76731d0a75a20d42fed9b0ee2
FatBaconCheese <justinjxie@gmail.com>
2022-07-12 14:09:14 -0700

    Commit of Both Versions of Self-Made Random Connected Graph Generation

---
+++ generate_random_graph_v1.py
@@ -0,0 +1,19 @@
+import igraph as ig
+import random
+
+#This function generates a random graph from a given number of vertices and edges
+def generate_random_graph(n, m):
+
+    #Creation of Graph
+    g = ig.Graph(directed=False)
+    g.add_vertices(n)
+
+    #Continuously looping until the graph has the target number of edges and does not have any loops
+    while g.ecount() < m or g != g.simplify():
+        source = random.randint(0, n-1)
+        target = random.randint(0, n-1)
+        while target == source:
+            target = random.randint(0,n-1)   
+        g.add_edge(source, target)
+    
+    return(g)
