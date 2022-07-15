a8e24d2c44c8cef529f055d955a0fd422cbbf551
FatBaconCheese <justinjxie@gmail.com>
2022-07-15 10:07:20 -0700

    Finds longest path in a graph

--- find_longest_path.py
+++ find_longest_path.py
@@ -1,22 +1,24 @@
+#Finds longest path in an unconnected and unweighted graph
+#Justin Xie 2022
+
 import igraph as ig
 
 #This function finds the longest simple path in a graph by looping through a list of all simple paths and returns the length of the longest path 
 def find_longest_path(g):
-    path_set = []
+    paths_list = []
 
     for i in range(len(g.vs)):
-        path_set = path_set + g.get_all_simple_paths(g.vs[i]) #function from igraph
+        paths_list = paths_list + g.get_all_simple_paths(g.vs[i]) #function from igraph
 
     longest = 0
-    all_longest_paths = []
-    for i in range(len(path_set)):
-        if len(path_set[i]) > longest:
-            longest = len(path_set[i])
-            all_longest_paths = [path_set[i]]
-        elif len(path_set[i]) == longest:
-            all_longest_paths.append(path_set[i])
+    longest_path = []
+    for path in paths_list:
+        if len(path) > longest:
+            longest = len(path)
+            longest_path = path
 
     if longest == 0:
-        return 0, all_longest_paths
+        return None
     else:
-        return longest - 1, all_longest_paths
+        return longest - 1, longest_path
+
