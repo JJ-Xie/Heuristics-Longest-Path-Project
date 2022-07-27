32b26a0011c10a3198df3306867ff5968b8159c0
FatBaconCheese <justinjxie@gmail.com>
2022-07-27 10:22:44 -0700

    Added short comments to heuristics init file

--- heuristics/__init__.py
+++ heuristics/__init__.py
@@ -1,5 +1,10 @@
+# Init file for heuristics
+# Justin Xie 2022
+
 from . import greedy, dfs_greedy, prune_central, stretch_total_graph_periphery, stretch_lowest_node_periphery
 
+# Allows benchmark suite to call all functions 
+# for comparison
 all = [
     greedy.greedy_longest_path, 
     dfs_greedy.dfs_greedy_longest_path,
@@ -8,6 +13,7 @@ all = [
     stretch_lowest_node_periphery.stretch_nodes_longest_path
     ]
 
+# Allows benchmark suite to call single functions
 run_greedy = greedy.greedy_longest_path
 run_dfs_greedy = dfs_greedy.dfs_greedy_longest_path
 run_prune_central = prune_central.prune_central_longest_path
